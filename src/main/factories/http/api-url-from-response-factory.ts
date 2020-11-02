41928ddee0d308d71371f3017729468b15ffb3d9
Matthaus Brandão <mathaus_soares07@hotmail.com>
2020-11-02 02:25:32 -0300

    refactor: refatoring hash for template string

--- src/main/factories/http/api-url-from-response-factory.ts
+++ src/main/factories/http/api-url-from-response-factory.ts
@@ -4,7 +4,7 @@ const PUBLIC_KEY = '3602c803495ec3d9a7d4422246aad37f'
 const PRIVATE_KEY = '9fb5758b56efdb5da62f10450983f9b2d10038f9'
 const timestamp = Number(new Date())
 const hash = md5.create()
-hash.update(timestamp + PRIVATE_KEY + PUBLIC_KEY)
+hash.update(`${timestamp}${PRIVATE_KEY}${PUBLIC_KEY}`)
 
 export const getApiUrlFromTheResponse = (apiUrl): string => {
   return `${apiUrl}&ts=${timestamp}&apikey=3602c803495ec3d9a7d4422246aad37f&hash=${hash}`
