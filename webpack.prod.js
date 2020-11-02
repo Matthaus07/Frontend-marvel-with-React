d551641644dd0333de1d6096a4ea32f6a9b90b09
Matthaus Brandão <mathaus_soares07@hotmail.com>
2020-11-02 13:48:36 -0300

    fix: ajust webpack production

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,29 +8,36 @@ const { merge } = require('webpack-merge')
 module.exports = merge(common, {
   mode: 'production',
   module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            }
-          },
-          {
-            loader: 'sass-loader'
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    },
+    {
+      test: /\.(jpe?g|gif|png|svg)$/i,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 10000
           }
-        ]
-      }]
+        }
+      ]
+    },
+    {
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        options: {
+          modules: true
+        }
+      }, {
+        loader: 'sass-loader'
+      }
+      ]
+    }]
   },
   externals: {
     react: 'React',
