c488ef723775718c27f74aeb2ec293fa7195cfe2
Matthaus Brandão <mathaus_soares07@hotmail.com>
2020-11-02 15:56:28 -0300

    fix: update api key for marvel

--- src/main/factories/http/api-url-factory.ts
+++ src/main/factories/http/api-url-factory.ts
@@ -1,7 +1,7 @@
 import md5 from 'js-md5'
 
-const PUBLIC_KEY = '83d65c3fe4e9bd364cd51734e06563d8'
-const PRIVATE_KEY = 'b7bb154d010479af8fd6279b371adb73472d4a83'
+const PUBLIC_KEY = '1d72a4d42c186726c707c4900f62845c'
+const PRIVATE_KEY = 'e2e32d0cf364ee496ef001282c4f1cdf846f4f2f'
 const times = Number(new Date())
 const hashs = md5.create()
 hashs.update(`${times}${PRIVATE_KEY}${PUBLIC_KEY}`)
